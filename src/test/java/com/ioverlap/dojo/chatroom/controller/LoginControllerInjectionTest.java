84376b0a140a1a7e9c1a98027814f16c1ee092d1
jnyupanqui <jnyupanqui@hotmail.com>
2020-02-02 19:50:53 -0800

    update tests. login test

--- src/test/java/com/ioverlap/dojo/chatroom/controller/LoginControllerInjectionTest.java
+++ src/test/java/com/ioverlap/dojo/chatroom/controller/LoginControllerInjectionTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,5 +28,49 @@ public class LoginControllerInjectionTest {
                 .andExpect(MockMvcResultMatchers.view().name("login"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.model().hasNoErrors());
+    }
+
+    @Test
+    public void testLoginInputLessLength() throws Exception {
+        String username = "kk";
+        mockMvc.perform(MockMvcRequestBuilders.post("/")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED).param("username", username))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.view().name("login"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().hasErrors());
+    }
+
+    @Test
+    public void testLoginInputGreaterLength() throws Exception {
+        String username = "supercalifragilisticexpialidocious";
+        mockMvc.perform(MockMvcRequestBuilders.post("/")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED).param("username", username))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.view().name("login"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().hasErrors());
+    }
+
+    @Test
+    public void testLoginInputEmpty() throws Exception {
+        String username = "";
+        mockMvc.perform(MockMvcRequestBuilders.post("/")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED).param("username", username))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.view().name("login"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().hasErrors());
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        String username = "pepe";
+        mockMvc.perform(MockMvcRequestBuilders.post("/").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("username", username))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.view().name("chat"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().hasNoErrors())
+                .andExpect(MockMvcResultMatchers.model().attribute("user", new User(username)));
     }
 }
