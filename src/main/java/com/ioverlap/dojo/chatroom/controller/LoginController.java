00086e2e73672bea1666f0ef799e6a290ff18aa6
jnyupanqui <jnyupanqui@hotmail.com>
2020-02-04 21:52:10 -0800

    send websocket url from controller

--- src/main/java/com/ioverlap/dojo/chatroom/controller/LoginController.java
+++ src/main/java/com/ioverlap/dojo/chatroom/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.ioverlap.dojo.chatroom.controller;
 
 import com.ioverlap.dojo.chatroom.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,10 +18,11 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String login(@Valid User user, BindingResult bindingResult) {
+    public String login(@Valid User user, BindingResult bindingResult, Model model) {
         if(bindingResult.hasErrors()) {
             return "login";
         }
+        model.addAttribute("webSocketUrl", "ws://localhost:8080/chat/");
         return "chat";
     }
 
